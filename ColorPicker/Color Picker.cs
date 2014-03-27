bf4531c53c24f56ff577bf853573dedb0e6e7835
CBickel87 <CBickel87@gmail.com>
2014-03-27 04:24:04 -0400

    First Commit
    Uploading all my files for color picker program

---
+++ ColorPicker/Color Picker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // PictureBox: This grabs the pixel color and sends that data to the text labels
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                Bitmap bmp = (Bitmap)colorwheel.Image;
+                Color clr = bmp.GetPixel(e.X, e.Y);
+                lbHex.Text = clr.Name;
+                lbR.Text = clr.R.ToString();
+                lbG.Text = clr.G.ToString();
+                lbB.Text = clr.B.ToString();
+                selector.BackColor = clr;
+            }
+            catch
+            {
+
+            }
+        }
+
+        // ColorWheel: This grabs the pixel color and sends that data to the text labels
+        private void colorbox_MouseMove(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                Bitmap bmp = (Bitmap)colorbox.Image;
+                Color clr = bmp.GetPixel(e.X, e.Y);
+                lbHex.Text = clr.Name;
+                lbR.Text = clr.R.ToString();
+                lbG.Text = clr.G.ToString();
+                lbB.Text = clr.B.ToString();
+                selector.BackColor = clr;
+            }
+            catch
+            {
+
+            }
+      
+        }
+    }
+}
